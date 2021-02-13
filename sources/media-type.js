09a1b9ce96b5c295f96f8a5b943e95bd7a4d1cfc
Serge Tsyba <tsyba@me.com>
2021-02-13 08:34:09 +0200

    Implemented MediaType formatting.

--- sources/media-type.js
+++ sources/media-type.js
@@ -5,4 +5,17 @@ export default class MediaType {
 		this.suffix = parameters.suffix || null
 		this.parameters = parameters.parameters || {}
 	}
+
+	get formatted() {
+		let formatted = this.type + '/' + this.subtype
+		if (this.suffix != null) {
+			formatted += '+' + this.suffix
+		}
+		for (const parameter in this.parameters) {
+			formatted += '; ' + parameter
+				+ '=' + this.parameters[parameter]
+		}
+
+		return formatted
+	}
 }
--- tests/media-type-tests.js
+++ tests/media-type-tests.js
@@ -2,7 +2,7 @@ import MediaType from '../sources/media-type.js'
 import {strict as assert} from 'assert'
 
 describe('MediaType', () => {
-	describe('creates media type', () => {
+	describe('creates media type with parameters', () => {
 		it('with application type when type is not specified', () => {
 			const mediaType = new MediaType({
 				subtype: 'vnd.company.content',
@@ -50,6 +50,75 @@ describe('MediaType', () => {
 			})
 
 			assert.deepEqual(mediaType.parameters, {})
+		})
+	})
+
+	describe('formats media type', () => {
+		it('full', () => {
+			const mediaType = new MediaType({
+				type: 'application',
+				subtype: 'vnd.company.content',
+				suffix: 'format',
+				parameters: {
+					param1: 'value1',
+					param2: 'value2'
+				}
+			})
+
+			assert.equal(mediaType.formatted,
+				'application/vnd.company.content+format; ' +
+				'param1=value1; param2=value2')
+		})
+
+		it('with wildcard subtype', () => {
+			const mediaType = new MediaType({
+				type: 'application',
+				subtype: '*',
+				suffix: 'format',
+				parameters: {
+					param1: 'value1',
+					param2: 'value2'
+				}
+			})
+
+			assert.equal(mediaType.formatted,
+				'application/*+format; ' +
+				'param1=value1; param2=value2')
+		})
+
+		it('without suffix', () => {
+			const mediaType = new MediaType({
+				type: 'application',
+				subtype: 'vnd.company.content',
+				parameters: {
+					param1: 'value1',
+					param2: 'value2'
+				}
+			})
+
+			assert.equal(mediaType.formatted,
+				'application/vnd.company.content; param1=value1; param2=value2')
+		})
+
+		it('without parameters', () => {
+			const mediaType = new MediaType({
+				type: 'application',
+				subtype: 'vnd.company.content',
+				suffix: 'format'
+			})
+
+			assert.equal(mediaType.formatted,
+				'application/vnd.company.content+format')
+		})
+
+		it('minimal', () => {
+			const mediaType = new MediaType({
+				type: 'application',
+				subtype: 'vnd.company.content'
+			})
+
+			assert.equal(mediaType.formatted,
+				'application/vnd.company.content')
 		})
 	})
 })
