4d644142117562e20fc3c74c7c144efdb6edc20d
Serge Tsyba <serge.tsyba@icloud.com>
2021-03-14 21:27:47 +0200

    Updated package.json files entry with whitelisted sources.

---
+++ index.js
@@ -0,0 +1,7 @@
+import MediaType from './sources/media-type.js'
+import ParseError from './sources/parse-error.js'
+
+export {
+	MediaType,
+	ParseError
+}
--- sources/index.js
+++
@@ -1,7 +0,0 @@
-import MediaType from './media-type'
-import ParseError from './parse-error'
-
-export {
-	MediaType,
-	ParseError
-}
